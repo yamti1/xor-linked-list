2fa2a6006823e7be4c48daeaeb5b1a9bd8bb5e2e
yam t <yam.tirosh@gmail.com>
2019-10-21 16:10:45 +0300

    Implement XorLinkedNode

--- xor-linked-list/xor-linked-list/lib/XorLinkedNode.cpp
+++ xor-linked-list/xor-linked-list/lib/XorLinkedNode.cpp
@@ -1,5 +1,15 @@
 #include "XorLinkedNode.h"
+#include "Utils.h"
 
-XorLinkedNode* XorLinkedNode::getNextOrPrev(XorLinkedNode* other) {
-	return (XorLinkedNode*)(other ^ this->both);
+XorLinkedNode::XorLinkedNode(int value, XorLinkedNode* next, XorLinkedNode* prev) {
+	this->__value = value;
+	this->set_both(prev, next);
+}
+
+XorLinkedNode* XorLinkedNode::get_next_or_prev(XorLinkedNode* other) {
+	return to_ptr(to_intptr(other) ^ this->__both);
+}
+
+void XorLinkedNode::set_both(XorLinkedNode* prev, XorLinkedNode* next) {
+	this->__both = to_intptr(next) ^ to_intptr(prev);
 }
--- xor-linked-list/xor-linked-list/lib/XorLinkedNode.h
+++ xor-linked-list/xor-linked-list/lib/XorLinkedNode.h
@@ -1,15 +1,16 @@
 #pragma once
+#include <cstdint>
 
 class XorLinkedNode
 {
 private:
-	bool both;
-	int value;
+	std::intptr_t __both;
+	int __value;
 
 public:
 	XorLinkedNode(int value, XorLinkedNode* next, XorLinkedNode* prev);
 
-	XorLinkedNode* getNextOrPrev(XorLinkedNode* other);
-	void setBoth(XorLinkedNode* prev, XorLinkedNode* next);
+	XorLinkedNode* get_next_or_prev(XorLinkedNode* other);
+	void set_both(XorLinkedNode* prev, XorLinkedNode* next);
 };
 
