d89724e99244007b820b9bf26dfb396471a103cf
yam t <yam.tirosh@gmail.com>
2019-11-01 17:35:42 +0200

    Minor fix to Utils

--- xor-linked-list/xor-linked-list/lib/Utils.cpp
+++ xor-linked-list/xor-linked-list/lib/Utils.cpp
@@ -7,6 +7,6 @@ std::intptr_t to_intptr(XORLinkedNode* ptr) {
 	return reinterpret_cast<std::intptr_t>(ptr);
 }
 
-XORLinkedNode* to_ptr(std::intptr_t &intptr) {
+XORLinkedNode* to_ptr(std::intptr_t intptr) {
 	return reinterpret_cast<XORLinkedNode*>(intptr);
 }
