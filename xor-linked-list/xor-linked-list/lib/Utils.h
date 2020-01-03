a88d976d4cbe67bd0bfb3fc0919a4982772685d6
yam t <yam.tirosh@gmail.com>
2020-01-03 14:43:40 +0200

    Fix `advance_node_ptrs` params should've been references to pointers instead of pointers

--- xor-linked-list/xor-linked-list/lib/Utils.h
+++ xor-linked-list/xor-linked-list/lib/Utils.h
@@ -5,4 +5,4 @@ std::intptr_t to_intptr(XORLinkedNode* ptr);
 
 XORLinkedNode* to_ptr(std::intptr_t intptr);
 
-void advance_node_ptrs(XORLinkedNode* current_node, XORLinkedNode* prev_node);
+void advance_node_ptrs(XORLinkedNode*& current_node, XORLinkedNode*& prev_node);
