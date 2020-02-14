ca9b3209314474a8045735d531222348842beb6f
yamti1 <30972111+yamti1@users.noreply.github.com>
2020-02-14 18:47:29 +0200

    Generic Typing (#12)
    * Add new files to project
    
    * WIP: Attempt conversion to template
    
    * Fix template conversion of Iterator
    
    * Fix std includes in XORLinkedList
    
    * Fix template conversion in XORLinkedNode

--- xor-linked-list/xor-linked-list/lib/Utils.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "XorLinkedNode.h"
-
-std::intptr_t to_intptr(XORLinkedNode* ptr);
-
-XORLinkedNode* to_ptr(std::intptr_t intptr);
-
-void advance_node_ptrs(XORLinkedNode*& current_node, XORLinkedNode*& prev_node);
--- xor-linked-list/xor-linked-list/lib/XorLinkedList.h
+++ xor-linked-list/xor-linked-list/lib/XorLinkedList.h
@@ -1,25 +1,99 @@
 #pragma once
+#include <stdexcept>
+#include <string>
+
 #include "XORLinkedListHandle.h"
 #include "XORLinkedListIterator.h"
 
+template <class T>
 class XORLinkedList
 {
 private:
-	XORLinkedListHandle __list_handle;
+	XORLinkedListHandle<T> __list_handle;
 	int __length;
 
+	// Get the value stored at index, starting from edge_node.
+	// index = 0 means the value at the edge node,
+	// index = 1 means the one value ofter that, etc.
+	T __get(XORLinkedNode<T>* const edge_node, const unsigned int index) {
+		if (nullptr == edge_node) {
+			throw std::invalid_argument("Got nullptr as the edge node");
+		}
+
+		XORLinkedNode<T>* current_node = edge_node;
+		XORLinkedNode<T>* prev_node = nullptr;
+		XORLinkedNode<T>* tmp_node = current_node;
+
+		for (size_t i = 0; i < index; i++)
+		{
+			advance_node_ptrs<T>(current_node, prev_node);
+
+			// Null means the list is over
+			if (current_node == nullptr) {
+				throw std::out_of_range("Attempted access to index " + std::to_string(index) + " in a list of length " + std::to_string(i + 1));
+			}
+		}
+
+		return current_node->get_value();
+	}
+
 public:
-	XORLinkedList();
+	XORLinkedList() :
+		__list_handle(XORLinkedListHandle<T>()),
+		__length()
+	{}
 
-	int get_length();
+	int get_length() {
+		return this->__length;
+	}
 
-	int get(const unsigned int& index);
-	int get_reverse(const unsigned int& index);
-	void add(int value);
+	// Get the value stored at index.
+	// index = 0 means the first value.
+	T get(const unsigned int& index) {
+		return __get(this->__list_handle.get_first(), index);
+	}
 
-	int operator[](int index);
+	// Get the value stored at index, but reversed.
+	// index = 0 means the last value, 
+	// index = 1 means the value one before the last, etc.
+	T get_reverse(const unsigned int& index)
+	{
+		return __get(this->__list_handle.get_last(), index);
+	}
 
-	XORLinkedListIterator iter();
-	XORLinkedListIterator reverse_iter();
+	void add(T value) {
+		this->__list_handle.add(value);
+		this->__length++;
+	}
+
+	// Get the value at the given index.
+	// May be given negative indexes for reverse indexing,
+	// where [-1] means the last value, [-2] means the value before that, etc.
+	T operator[](const int index)
+	{
+		if (0 <= index) {
+			return this->get(index);
+		}
+
+		try {
+			// Negative indexes start at -1 but get_reverse indexes start at 0.
+			unsigned int reverse_index = std::abs(index) - 1;
+			return this->get_reverse(reverse_index);
+		}
+		catch (std::out_of_range) {
+			// Change the error message to match the original index
+			throw std::out_of_range("Attempted access to index " + std::to_string(index) + " in a list of length " + std::to_string(this->get_length()));
+		}
+	}
+
+	XORLinkedListIterator<T> iter()
+	{
+		return XORLinkedListIterator<T>(this->__list_handle.get_first());
+	}
+
+	XORLinkedListIterator<T> reverse_iter()
+	{
+		return XORLinkedListIterator<T>(this->__list_handle.get_last());
+	}
 };
 
