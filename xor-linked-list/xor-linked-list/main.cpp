1c9a144b12f5de1ff07f13dfec69484613feb30e
yam t <yam.tirosh@gmail.com>
2020-02-29 18:48:20 +0200

    Add updated main file

--- xor-linked-list/xor-linked-list/main.cpp
+++ xor-linked-list/xor-linked-list/main.cpp
@@ -1,13 +1,40 @@
 #include <iostream>
+#include <exception>
+
+#include "lib/XORLinkedList.h"
 
 int main()
 {
-	int a = 0;
-	int b = 1;
-	int* a_ptr = &a;
-	int* b_ptr = &b;
+	int arr[] = { 1, 2, 3 };
+	//XORLinkedList<int> list({ 1, 2, 3, 4, 5 });
+	XORLinkedList<int> list(arr);
 
-	int* x_ptr = a_ptr ^ b_ptr;
+	for (int value : list.iter()) {
+		std::cout << value << ", ";
+	}
 
-	std::cout << x_ptr << std::endl;
+	std::cout << std::endl;
+
+	for (int value : list.reverse_iter()) {
+		std::cout << value << ", ";
+	}
+
+
+	std::cout << "list.get(1) = " << list.get(1) << std::endl;
+	std::cout << "list.get_reverse(1) = " << list.get_reverse(1) << std::endl;
+
+	std::cout << "list[1] = " << list[1] << std::endl;
+	std::cout << "list[-2] = " << list[-2] << std::endl;
+
+	XORLinkedList<std::string> str_list;
+	str_list.add("One");
+	str_list.add("Two");
+	str_list.add("Three");
+
+	for (std::string str : str_list.iter())
+	{
+		std::cout << str << ", ";
+	}
+
+	std::cout << str_list[2] << std::endl;
 }
